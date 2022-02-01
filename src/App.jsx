c448c1b29437af6fb15cfda451aeabfb0b5e24f3
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2022-02-01 00:53:33 -0800

    fetched data and set up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { Switch, Route, BrowserRouter, Link } from 'react-router-dom';
+import HyruleList from './components/HyruleList/HyruleList';
+
 import Home from './views/Home/Home';
 
 export default function App() {
   return (
-    <>
-      <h1>Hyrule Compendium</h1>
-      <Home />
-    </>
+    <BrowserRouter>
+      <header>
+        <h1>Hyrule Compendium</h1>
+        <Link to="/category/creatures">Creatures</Link>
+        <Link to="/category/monsters">Monsters</Link>
+        <Link to="/category/equipment">Equipment</Link>
+      </header>
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/category/:category">
+          <HyruleList />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,8 +1,14 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
 import HyruleList from '../../components/HyruleList/HyruleList';
 import { fetchData } from '../../services/hyrule';
 
 export default function Home() {
+  // const [category, setCategory] = useState();
+  // const { category } = useParams();
+  // const params = useParams();
+  // console.log('params', params);
+
   useEffect(() => {
     const fetchHyrule = async () => {
       const data = await fetchData();
@@ -14,7 +20,6 @@ export default function Home() {
   return (
     <>
       <div>Home</div>
-      <HyruleList />
     </>
   );
 }
