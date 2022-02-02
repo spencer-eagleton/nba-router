20b66457fd8ac44c406cf51959d3db7d6897ef10
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2022-02-01 16:02:22 -0800

    got category data

--- src/components/HyruleList/HyruleList.jsx
+++ src/components/HyruleList/HyruleList.jsx
@@ -1,16 +1,18 @@
 import { fetchData } from '../../services/hyrule';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 export default function HyruleList() {
+  const [hyruleData, setHyruleData] = useState([]);
   const { category } = useParams();
   console.log(category);
   useEffect(() => {
     const fetchHyrule = async () => {
-      const data = await fetchData();
+      const data = await fetchData(category);
       console.log(data);
+      setHyruleData(data);
     };
     fetchHyrule();
-  }, []);
+  }, [category]);
 
   return (
     <>
--- src/services/hyrule.js
+++ src/services/hyrule.js
@@ -1,6 +1,8 @@
-export const fetchData = async () => {
-  const resp = await fetch('https://botw-compendium.herokuapp.com/api/v2/all');
+export async function fetchData(category) {
+  const resp = await fetch(
+    `https://botw-compendium.herokuapp.com/api/v2/category/${category}`
+  );
   const data = await resp.json();
   console.log(data);
   return data;
-};
+}
