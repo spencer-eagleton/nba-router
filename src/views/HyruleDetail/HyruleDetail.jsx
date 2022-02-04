c6159421711b038db6b7da6b1600b63002de390f
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2022-02-03 19:47:43 -0800

    added a u

--- src/views/HyruleDetail/HyruleDetail.jsx
+++ src/views/HyruleDetail/HyruleDetail.jsx
@@ -2,11 +2,13 @@ import DetailCard from '../../components/DetailCard/DetailCard';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchDataById } from '../../services/hyrule';
+import { useHistory } from 'react-router-dom';
 
 export default function HyruleDetail() {
   const [loading, setLoading] = useState(true);
   const [hyruleEntry, setHyruleEntry] = useState([]);
   const { entry } = useParams();
+  const history = useHistory();
 
   useEffect(() => {
     const fetchHyruleById = async () => {
@@ -24,6 +26,13 @@ export default function HyruleDetail() {
     <>
       <h1>Hyrule Detail</h1>
       <DetailCard hyruleEntry={hyruleEntry} />
+      <button
+        onClick={() => {
+          history.push('/');
+        }}
+      >
+        Back to Home
+      </button>
     </>
   );
 }
